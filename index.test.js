66a54f8382b23c61406226e54cc49691e4cac420
Alex <alexburrows915@icloud.com>
2018-05-28 11:23:04 +0100

    add unit tests for index.js

---
+++ index.test.js
@@ -0,0 +1,9 @@
+const { five, board } = require('./index');
+
+test('checks if five exists.', () => {
+  expect(five).toBeDefined();
+});
+
+test('checks if board exists.' () => {
+  expect(board).toBeDefined();
+})
